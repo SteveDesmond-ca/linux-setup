1ee24b7c12089e3387eab983067607ab1ef73ffd
Steve Desmond <Steve@ecoAPM.com>
2021-11-11 12:00:30 -0500

    Fix git config by intending script run as non-root user

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-apt update
-apt full-upgrade -y
+sudo apt update
+sudo apt full-upgrade -y
 sudo apt-get install -y \
 	apt-transport-https \
 	wget \
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
 rm -f packages.microsoft.gpg
 
 wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo bash -
 
-apt update -y
-apt install -y \
+sudo apt update -y
+sudo apt install -y \
 	code \
 	dotnet-sdk-3.1 \
 	dotnet-sdk-5.0 \
@@ -28,12 +28,12 @@ apt install -y \
 	nodejs \
 	remmina \
 
-npm i -g npm yarn
+sudo npm i -g npm yarn
 
-curl -L https://get.docker.com/ | bash
-usermod -aG docker vtsv
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod a+x /usr/local/bin/docker-compose
+curl -L https://get.docker.com/ | sudo bash
+sudo usermod -aG docker vtsv
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
 
 git config --global user.name "Steve Desmond"
 git config --global user.email "steve@vtsv.ca"
@@ -46,5 +46,5 @@ git config --global alias.p "pull --prune"
 git config --global alias.ca "commit --amend --no-edit"
 git config --global alias.pf "push --force-with-lease"
 
-apt autoremove -y
-apt autoclean -y
+sudo apt autoremove -y
+sudo apt autoclean -y
